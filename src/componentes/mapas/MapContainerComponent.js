26e7c3d61f1ad2292cf097ff251763f728ca1ea7
Elio Nicolas <baigorriaen83@gmail.com>
2025-11-13 21:49:02 -0300

    frontend en Render2

--- src/componentes/mapas/MapContainerComponent.js
+++ src/componentes/mapas/MapContainerComponent.js
@@ -144,7 +144,7 @@ const MapContainerComponent = () => {
 
   const fetchBuses = async () => {
     try {
-      const response = await fetch("https://mibus-frontend.onrender.com/buses");
+      const response = await fetch("https://mibus-backend.onrender.com/buses");
       if (!response.ok) throw new Error(`HTTP Error: ${response.status}`);
       const data = await response.json();
       setBuses(data);
--- src/pages/Logins.js
+++ src/pages/Logins.js
@@ -19,7 +19,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch("https://mibus-frontend.onrender.com/api/users/signin", { // se cambio de 3001 a 4001
+      const res = await fetch("https://mibus-backend.onrender.com/api/users/signin", { // se cambio de 3001 a 4001
 
         method: "POST",
         headers: { "Content-Type": "application/json" },
